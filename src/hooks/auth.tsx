82f26a22f059a5082858a6c9d3e5f1526b52d716
Luiz <luiz.ado@hotmail.com>
2020-11-01 01:57:12 -0300

    Listing Providers on the application (It's not working, needed to be fixed).

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -40,6 +40,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if(token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) })
       }
 
@@ -59,7 +61,9 @@ const AuthProvider: React.FC = ({ children }) => {
 
     await AsyncStorage.multiSet([
       ['@GoBarber:token', token], ['@GoBarber:user', JSON.stringify(user)]
-    ])
+    ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user } as AuthState);
   }, []);
