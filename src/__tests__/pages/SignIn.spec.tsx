ebfeb7cc723bb15b6ca40cb3a9f2fbd72248666f
Luiz <luiz.ado@hotmail.com>
2020-11-13 19:50:22 -0300

    Adjusted some errors that was building wrongly the AsyncStorage mock. Finished the first test.

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -3,6 +3,10 @@ import { render } from 'react-native-testing-library';
 
 import SignIn from '../../pages/SignIn';
 
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
+
 describe('SingIn page', () => {
   it('should contains email/password inputs', () => {
     const { getByPlaceholder } = render(<SignIn />);
